b45820058aa033b6a5788e8dbbbc3710d51cda16
pavanpurohit47 <83494516+pavanpurohit47@users.noreply.github.com>
2022-09-21 10:27:43 +0530

    Update bitwise_operations.py

--- bitwise_operations.py
+++ bitwise_operations.py
@@ -3,7 +3,7 @@
 import cv2 as cv
 import numpy as np
 
-blank = np.zeros((400,400), dtype='uint8')
+blank = np.zeros((400,400), dtype='uint8')  
 
 rectangle = cv.rectangle(blank.copy(), (30,30), (370,370), 255, -1)
 circle = cv.circle(blank.copy(), (200,200), 200, 255, -1)
